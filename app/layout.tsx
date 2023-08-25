a4044f0286f973666e0cdd4da488709454a0ac99
HabibMollah <hrmollah2002@gmail.com>
2023-08-25 19:31:54 +0600

    add dark theme to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Habib Mollah',
-  description: 'Habib Mollah is a professional front end engineer',
+  title: "Habib Mollah",
+  description: "Habib Mollah is a professional front end engineer",
 };
 
 export default function RootLayout({
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
