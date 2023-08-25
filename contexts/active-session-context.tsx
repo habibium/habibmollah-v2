9b09123fdd9dc6f52bf7fe020c7de52edf6f86e0
HabibMollah <hrmollah2002@gmail.com>
2023-08-26 02:04:24 +0600

    add add active-session-context

---
+++ contexts/active-session-context.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { SectionName } from "@/lib/types";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+type ActiveSectionContextProviderProps = {
+  children: ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: Dispatch<SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: Dispatch<SetStateAction<number>>;
+};
+
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within a ActiveSectionContextProvider",
+    );
+  }
+  return context;
+}
