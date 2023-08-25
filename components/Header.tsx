271261ea6dd84c6c8acb6accdc852dc674339a17
HabibMollah <hrmollah2002@gmail.com>
2023-08-26 02:47:07 +0600

    improve header[make active section functional]

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,34 @@
 
 import { useActiveSectionContext } from "@/contexts/active-session-context";
 import { links } from "@/lib/data";
+import clsx from "clsx";
 import Link from "next/link";
 
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
 
+  console.log(Date.now());
   return (
     <header className="relative z-[999]">
       <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[inital] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-            <Link href={link.hash} key={link.name}>
+            <Link
+              className={clsx(
+                "flex w-full items-center justify-center p-3 duration-300 hover:text-gray-950 dark:text-gray-400 dark:hover:text-gray-100",
+                {
+                  "text-gray-950 dark:text-gray-100":
+                    activeSection === link.name,
+                },
+              )}
+              href={link.hash}
+              key={link.name}
+              onClick={() => {
+                setActiveSection(link.name);
+                setTimeOfLastClick(Date.now());
+              }}
+            >
               {link.name}
             </Link>
           ))}
