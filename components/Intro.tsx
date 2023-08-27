fbd13597334035f195d4a68f7e72acff77cc2067
HabibMollah <hrmollah2002@gmail.com>
2023-08-27 21:04:48 +0600

    finish intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,3 +1,84 @@
+"use client";
+
+import { useActiveSectionContext } from "@/contexts/active-session-context";
+import useSectionInView from "@/lib/hooks";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
 export default function Intro() {
-  return <div>Intro</div>;
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] scroll-mt-[100rem] text-center sm:mb-0"
+    >
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Habib Mollah</span> I'm a{" "}
+        <span className="font-bold">front-end engineer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>
+      </motion.h1>
+      <motion.div
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none duration-300 hover:scale-110 focus:scale-110 active:scale-105"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 transition group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          className="borderBlack group flex cursor-pointer items-center gap-2 rounded-full bg-white px-7 py-3 outline-none duration-300 hover:scale-110 focus:scale-110 active:scale-105 dark:bg-white/10"
+          href="/resume-habib-mollah.pdf"
+          download
+        >
+          Download Resume{" "}
+          <HiDownload className="opacity-60 transition group-hover:translate-x-1" />
+        </a>
+
+        <a
+          className="borderBlack flex cursor-pointer items-center gap-2 rounded-full bg-white p-4 text-gray-700 duration-300 hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/habibmollah"
+          target="_blank"
+          className="borderBlack flex cursor-pointer items-center gap-2 rounded-full bg-white p-4 text-gray-700 duration-300 hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
 }
