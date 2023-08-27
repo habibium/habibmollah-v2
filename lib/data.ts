95c7e6f25758446410cac64501a2dbe4001a18b6
HabibMollah <hrmollah2002@gmail.com>
2023-08-28 00:45:59 +0600

    add experience

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,9 @@
+import { createElement } from "react";
+
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
@@ -73,4 +79,31 @@ export const skillsData = [
   { name: "Python", icon: "python-icon-url" },
   { name: "Django", icon: "django-icon-url" },
   { name: "Framer Motion", icon: "framer-motion-icon-url" },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: createElement(FaReact),
+    date: "2021 - present",
+  },
 ] as const;
