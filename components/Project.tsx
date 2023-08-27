eca2236d6217108ce6795e0693306c3a36219b3d
HabibMollah <hrmollah2002@gmail.com>
2023-08-27 23:32:16 +0600

    finish project

--- components/Project.tsx
+++ components/Project.tsx
@@ -28,17 +28,44 @@ export default function Project({ title, description, tags, imageUrl }: Props) {
       }}
       className="group mb-3 last:mb-0 sm:mb-8"
     >
-      <section>
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <ul>
+      <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
+        <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            {description}
+          </p>
+          <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
             {tags.map((tag) => (
-              <li key={tag}>{tag}</li>
+              <li
+                className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] font-medium uppercase tracking-wider text-white dark:text-white/70"
+                key={tag}
+              >
+                {tag}
+              </li>
             ))}
           </ul>
         </div>
-        <Image src={imageUrl} alt="Project image" quality={95} />
+        <Image
+          src={imageUrl}
+          alt="Project image"
+          quality={95}
+          className="absolute -right-40 top-8 hidden w-[28.25rem] rounded-t-lg shadow-2xl transition duration-700 
+          
+          group-even:-left-40 
+          
+          group-even:right-[initial] 
+          
+          group-hover:-translate-x-3
+          
+          group-hover:-translate-y-3 
+          
+          group-hover:-rotate-[15deg]
+
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:rotate-[15deg]
+          sm:block
+          "
+        />
       </section>
     </motion.div>
   );
