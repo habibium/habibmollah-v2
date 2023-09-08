6e9c17b58c428013f848e2a6914a5829db82c434
HabibMollah <hrmollah2002@gmail.com>
2023-09-08 10:01:35 +0600

    fade in the project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/lib/data";
 import { Fragment } from "react";
 import Project from "./Project";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.1);
@@ -12,13 +13,16 @@ export default function Projects() {
   return (
     <section id="projects" ref={ref} className="mb-28 scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
-      <div>
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+      >
         {projectsData.map((project, index) => (
           <Fragment key={index}>
             <Project {...project} />
           </Fragment>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 }
