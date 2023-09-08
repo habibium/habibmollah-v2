70f5d3bf6f1287cebe55e2890d068c7bebd1c147
HabibMollah <hrmollah2002@gmail.com>
2023-09-08 09:46:52 +0600

    add performance scores to about section

--- components/About.tsx
+++ components/About.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion";
 import SectionHeading from "./SectionHeading";
 import useSectionInView from "@/lib/hooks";
+import Image from "next/image";
 
 export default function About() {
   const { ref } = useSectionInView("About");
@@ -11,38 +12,45 @@ export default function About() {
     <motion.section
       ref={ref}
       id="about"
-      className="mb-28 max-w-[45rem] scroll-mt-28 text-center leading-8 sm:mb-40"
+      className="mb-28 max-w-[80rem] scroll-mt-28 sm:mb-40"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
+      <div className="flex flex-col-reverse items-center justify-between gap-8 lg:flex-row lg:gap-16">
+        <figure className="block max-w-[90vw] rounded-xl bg-[#222124] text-center sm:px-10 md:max-w-[60vw] md:px-24">
+          <Image
+            className="rounded-t-xl"
+            width={1075.5}
+            height={1018.5}
+            alt="performance scores of this portfolio"
+            src="/portfolio-performance-scores.webp"
+          />
+          <figcaption className="italic text-gray-400">
+            Performance scores of this website
+          </figcaption>
+        </figure>
+        <div className="text-center sm:text-xl">
+          <p>
+            I'm an experienced front-end developer and{" "}
+            <span className="bg-blue-400 px-1 font-medium text-white dark:bg-blue-500">
+              TypeScript
+            </span>{" "}
+            enthusiast.
+          </p>
+          <p>
+            My focus is on creating <strong>modern</strong>{" "}
+            <span className="font-bold text-green-500 dark:text-green-400">
+              high-performance
+            </span>{" "}
+            websites and apps that <strong>excel</strong> in{" "}
+            <span className="bg-pink-200 px-1 font-bold dark:bg-pink-500">
+              design, functionality, accessibility, and user experience.
+            </span>
+          </p>
+        </div>
+      </div>
     </motion.section>
   );
 }
