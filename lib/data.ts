31efdfc1048d3a1f92b06f52bfbc3bd3c8a2f2c5
HabibMollah <hrmollah2002@gmail.com>
2023-09-10 15:36:56 +0600

    update experiences

--- lib/data.ts
+++ lib/data.ts
@@ -143,27 +143,27 @@ export const skillsData = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Responsive Web Design Certification",
+    location: "freeCodeCamp.org",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: createElement(LuGraduationCap),
     date: "2019",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "JavaScript Algorithms and Data Structures Certification",
+    location: "freeCodeCamp.org",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: createElement(CgWorkAlt),
+    icon: createElement(LuGraduationCap),
     date: "2019 - 2021",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Front-End Development Libraries Certification",
+    location: "freeCodeCamp.org",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: createElement(FaReact),
+    icon: createElement(LuGraduationCap),
     date: "2021 - present",
   },
 ] as const;
