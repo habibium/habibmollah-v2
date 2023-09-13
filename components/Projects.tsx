1b31aa1443402a27b6e0bdd9a60df4d3d86326f6
HabibMollah <hrmollah2002@gmail.com>
2023-09-13 12:20:23 +0600

    remove show less

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,19 +3,13 @@
 import useSectionInView from "@/lib/hooks";
 import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/lib/data";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import Project from "./Project";
 import { motion } from "framer-motion";
 
 export default function Projects() {
   const [isExpanded, setIsExpanded] = useState(false);
   const { ref } = useSectionInView("Projects", 0.1);
-
-  useEffect(() => {
-    const projectsSection = document.getElementById("projects");
-
-    if (!isExpanded && projectsSection) projectsSection.scrollIntoView();
-  }, [isExpanded]);
 
   return (
     <section id="projects" ref={ref} className="mb-28 scroll-mt-28">
@@ -36,15 +30,18 @@ export default function Projects() {
               </Fragment>
             ))}
       </motion.div>
-      <button
-        id="show-all-button"
-        className="mx-auto mt-8 block rounded-xl bg-black px-3 py-1 text-lg font-medium text-white dark:bg-white dark:text-black"
-        onClick={() => {
-          setIsExpanded(!isExpanded);
-        }}
-      >
-        {isExpanded ? "Show less" : "Show all"}
-      </button>
+      {!isExpanded && (
+        <button
+          id="show-all-button"
+          className="mx-auto mt-8 block rounded-xl bg-black px-3 py-1 text-lg font-medium text-white dark:bg-white dark:text-black"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          {/* {isExpanded ? "Show less" : "Show all"} */}
+          Show all
+        </button>
+      )}
     </section>
   );
 }
