915ae1640996252a484f7bb43059bf5fb0f2a779
HabibMollah <habib.prodev@gmail.com>
2023-10-24 13:11:24 +0600

    update email

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -20,8 +20,8 @@ export default function Contact() {
 
       <p className="-mt-6 text-gray-700 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:hrmollah2002@gmail.com">
-          hrmollah2002@gmail.com
+        <a className="underline" href="mailto:habib.prodev@gmail.com">
+          habib.prodev@gmail.com
         </a>{" "}
         or through this form.
       </p>
