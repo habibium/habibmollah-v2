29f1c9377d123871fb2577026367c64b36a019da
habibium <habib.prodev@gmail.com>
2025-07-16 07:25:52 +0600

    fix: Update warning message and link to habibium.com for latest information

--- components/Warning.tsx
+++ components/Warning.tsx
@@ -3,23 +3,15 @@ export default function Warning() {
     <aside className="fixed top-0 z-[1000] w-full bg-yellow-300 text-black">
       <div className="mx-auto max-w-[80rem] px-1 py-2 text-center sm:px-4">
         <p>
-          This website is outdated. I'm working on a fresh rewrite in my free
-          time. For the latest data about me, here's my{" "}
+          🚨 This website is outdated. Please visit{" "}
           <a
             className="font-bold underline"
             target="_blank"
-            href="https://drive.google.com/file/d/1haGl_E24kii3MQ2sSUY6PjQsoYLBFUPZ/view?usp=sharing"
+            href="https://habibium.com"
           >
-            resume
+            habibium.com
           </a>{" "}
-          and{" "}
-          <a
-            className="font-bold underline"
-            target="_blank"
-            href="https://linkedin.com/in/habibium"
-          >
-            LinkedIn
-          </a>
+          for the latest info.
         </p>
       </div>
     </aside>
